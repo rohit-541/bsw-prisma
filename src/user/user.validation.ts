13f4dfb69ef7e6e371cb16225e70c1664cb995f2
rohit-541 <rohitkumar240iit@gmail.com>
2025-01-27 17:08:45 +0530

    Updated hostel enum

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -72,8 +72,8 @@ export class updateDTO{
 export class passdto{
   @IsNotEmpty()
   @IsString()
-  @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{12,64}$/,{
-    message:"Password should be atleast 12 character string with one upperCase ,one lowerCase,one Digit and one specialCharacter"
+  @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,64}$/,{
+    message:"Password should be atleast 8 character string with one upperCase ,one lowerCase,one Digit and one specialCharacter"
   })
   password:string
 
