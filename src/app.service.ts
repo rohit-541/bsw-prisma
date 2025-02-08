7cbfaf5f1243e7e825fc580e1df66a99e79cc129
rohit-541 <rohitkumar240iit@gmail.com>
2025-02-08 14:34:46 +0530

    Added Message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'This is Backend Portal for Aceadmic MentorShip. This is Currently working frontend is pending to host which we will host soon! please stay updated';
   }
 }
